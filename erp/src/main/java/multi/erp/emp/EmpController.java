9d7b9be27830355825d5e454ba6e839274f263cd
insunkang <cactus2378@naver.com>
2020-02-06 21:49:47 +0900

    ajax

--- erp/src/main/java/multi/erp/emp/EmpController.java
+++ erp/src/main/java/multi/erp/emp/EmpController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 //emp테이블에서 만들어지는 모든 내용에 대한 controller
@@ -43,7 +44,7 @@ public class EmpController {
 			HttpSession ses = request.getSession();
 			//2. 세션에 데이터 공유
 			ses.setAttribute("user", user);
-			viewName="index";
+			viewName="login/ok";
 		}else{
 			viewName="login";
 			mav.setViewName(viewName);
@@ -53,7 +54,7 @@ public class EmpController {
 		
 		System.out.println("==========로그인 성공 후 조회된 레코드로 만들어진 값"+user);
 		mav.addObject("user",user);
-		mav.setViewName("index");
+		mav.setViewName(viewName);
 		return mav;
 	}
 	@RequestMapping("/emp/logout.do")
@@ -65,5 +66,25 @@ public class EmpController {
 			session.invalidate();
 		}
 		return "redirect:/index.do";
+	}
+	@RequestMapping("/emp/insertView.do")
+	public String insertView() {
+		return "emp/insert";
+	}
+	// produces속성 : ajax요청 후 클라이언트로 전송할 데이터의 타입을 정의
+	//				 application/text는 ajax요청 후 클라이언트로 보내는
+	//				 응답메세지의 타입이 text라는 뜻
+	@RequestMapping(value= "/emp/idCheck.do",
+					method =RequestMethod.GET,
+					produces="application/text;charset=utf-8"	)
+	public @ResponseBody String idCheck(String id) {
+		boolean state = service.idCheck(id);
+		String result="";
+		if(state) {//이미 사용자가 입력한 아이디가 db에 저장되어 있다는 의미
+			result="사용 불가능한 아이디";
+		}else {
+			result="사용 가능한 아이디";
+		}
+		return result;
 	}
 }
